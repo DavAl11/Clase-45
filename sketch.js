41a3f78be2bf5dc580ca7c4ba256a96261a55fee
DavAl11 <92409328+DavAl11@users.noreply.github.com>
2022-04-28 17:58:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var player;
+
+function setup()
+{
+  createCanvas(windowWidth,windowHeight);
+  player = createSprite(windowWidth/2,windowHeight/2);
+}
+
+function draw() 
+{
+  background(54,143,257);
+  drawSprites();
+}
+
